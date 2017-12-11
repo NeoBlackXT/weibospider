1e5fd67d879a6f07ec335bb5f96629dab26b4eda
NeoBlackXT <neoblackxt@outlook.com>
2017-12-11 20:55:56 +0800

    增加 scrapy_redis模块，启用redis请求队列，redis Item pipeline

--- weibospider/settings.py
+++ weibospider/settings.py
@@ -30,7 +30,7 @@ DOWNLOAD_DELAY = 5
 CONCURRENT_REQUESTS_PER_IP = 2
 
 # Disable cookies (enabled by default)
-# COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 # TELNETCONSOLE_ENABLED = False
@@ -40,6 +40,7 @@ CONCURRENT_REQUESTS_PER_IP = 2
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
 # }
+
 
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
@@ -70,7 +71,8 @@ DOWNLOADER_MIDDLEWARES = {
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'weibospider.pipelines.WeiboPipeline': 300,
+    'scrapy_redis.pipelines.RedisPipeline': 300,
+    'weibospider.pipelines.WeiboPipeline': 310,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
@@ -125,9 +127,74 @@ IP_PROXY_POOL = [
 
 ]
 
-DUPEFILTER_CLASS = "weibospider.bloom_filter.BloomFilter"
+DUPEFILTER_CLASS = "weibospider.bloom_filter.RFPDupeFilter"
 DUPEFILTER_DEBUG = True
-REDIS_BLOOM_FILTER_HOST = '192.168.145.128'
-REDIS_BLOOM_FILTER_PORT = 6379
-REDIS_BLOOM_FILTER_DB = 0
-REDIS_BLOOM_FILTER_PASSWORD = 'k8JR4df0MVZmv7Ucezb9'
+
+
+# Enables scheduling storing requests queue in redis.
+SCHEDULER = "scrapy_redis.scheduler.Scheduler"
+
+# Ensure all spiders share same duplicates filter through redis.
+# DUPEFILTER_CLASS = "scrapy_redis.dupefilter.RFPDupeFilter"
+
+# Default requests serializer is pickle, but it can be changed to any module
+# with loads and dumps functions. Note that pickle is not compatible between
+# python versions.
+# Caveat: In python 3.x, the serializer must return strings keys and support
+# bytes as values. Because of this reason the json or msgpack module will not
+# work by default. In python 2.x there is no such issue and you can use
+# 'json' or 'msgpack' as serializers.
+# SCHEDULER_SERIALIZER = "scrapy_redis.picklecompat"
+
+# Don't cleanup redis queues, allows to pause/resume crawls.
+SCHEDULER_PERSIST = True
+
+# Schedule requests using a priority queue. (default)
+# SCHEDULER_QUEUE_CLASS = 'scrapy_redis.queue.PriorityQueue'
+
+# Alternative queues.
+# SCHEDULER_QUEUE_CLASS = 'scrapy_redis.queue.FifoQueue'
+# SCHEDULER_QUEUE_CLASS = 'scrapy_redis.queue.LifoQueue'
+
+# Max idle time to prevent the spider from being closed when distributed crawling.
+# This only works if queue class is SpiderQueue or SpiderStack,
+# and may also block the same time when your spider start at the first time (because the queue is empty).
+# SCHEDULER_IDLE_BEFORE_CLOSE = 10
+
+# Store scraped item in redis for post-processing.
+# ITEM_PIPELINES = {
+#     'scrapy_redis.pipelines.RedisPipeline': 300
+# }
+
+# The item pipeline serializes and stores the items in this redis key.
+# REDIS_ITEMS_KEY = '%(spider)s:items'
+
+# The items serializer is by default ScrapyJSONEncoder. You can use any
+# importable path to a callable object.
+# REDIS_ITEMS_SERIALIZER = 'json.dumps'
+
+# Specify the host and port to use when connecting to Redis (optional).
+# REDIS_HOST = 'localhost'
+# REDIS_PORT = 6379
+
+# Specify the full Redis URL for connecting (optional).
+# If set, this takes precedence over the REDIS_HOST and REDIS_PORT settings.
+# REDIS_URL = 'redis://user:pass@hostname:9001'
+REDIS_URL = 'redis://:k8JR4df0MVZmv7Ucezb9@192.168.145.128:6379'
+
+# Custom redis client parameters (i.e.: socket timeout, etc.)
+# REDIS_PARAMS  = {}
+# Use custom redis client class.
+# REDIS_PARAMS['redis_cls'] = 'myproject.RedisClient'
+
+# If True, it uses redis' ``SPOP`` operation. You have to use the ``SADD``
+# command to add URLs to the redis queue. This could be useful if you
+# want to avoid duplicates in your start urls list and the order of
+# processing does not matter.
+# REDIS_START_URLS_AS_SET = False
+
+# Default start urls key for RedisSpider and RedisCrawlSpider.
+# REDIS_START_URLS_KEY = '%(name)s:start_urls'
+
+# Use other encoding than utf-8 for redis.
+# REDIS_ENCODING = 'latin1'
