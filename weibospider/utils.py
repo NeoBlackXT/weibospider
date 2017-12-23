2fd78d65f49d956607c2771e27ad5ef7dde6d211
NeoBlackXT <neoblackxt@outlook.com>
2017-12-24 02:11:59 +0800

    增加 尾递归处理装饰器

--- weibospider/utils.py
+++ weibospider/utils.py
@@ -1,3 +1,4 @@
+import sys
 import json
 import logging
 import re
@@ -80,3 +81,44 @@ class CookieUtil:
             return lst
         else:
             raise RuntimeError('setcookie类型不为列表')
+
+
+class TailRecurseException(BaseException):
+    def __init__(self, args, kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+
+class TailRecursion:
+    # This program shows off a python decorator(
+    # which implements tail call optimization. It
+    # does this by throwing an exception if it is
+    # it's own grandparent, and catching such
+    # exceptions to recall the stack.
+
+    def tail_call_optimized(g):
+        """
+        This function decorates a function with tail call
+        optimization. It does this by throwing an exception
+        if it is it's own grandparent, and catching such
+        exceptions to fake the tail call optimization.
+
+        This function fails if the decorated
+        function recurses in a non-tail context.
+        """
+
+        def func(*args, **kwargs):
+            f = sys._getframe()
+            if f.f_back and f.f_back.f_back \
+                    and f.f_back.f_back.f_code == f.f_code:
+                raise TailRecurseException(args, kwargs)
+            else:
+                while 1:
+                    try:
+                        return g(*args, **kwargs)
+                    except TailRecurseException as e:
+                        args = e.args
+                        kwargs = e.kwargs
+
+        func.__doc__ = g.__doc__
+        return func
